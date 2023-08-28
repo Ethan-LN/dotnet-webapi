26620b0bcfd86e1e133ac42ed4f756df8a6c4a4c
Ethan-LN <et.l.nicholas@gmail.com>
2023-08-28 23:40:08 +1000

    temperate saving

---
+++ BuberDinner.Contracts/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,10 @@
+namespace BuberDinner.Contracts.Authentication;
+
+public record AuthenticationResponse(
+    Guid Id,
+    string Firstname,
+    string Lastname,
+    string Email,
+    string Password,
+    string Token
+);
