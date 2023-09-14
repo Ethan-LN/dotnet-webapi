685225a753a5689b4efc0159a760bf44969b4801
Ethan-LN <et.l.nicholas@gmail.com>
2023-09-14 22:03:23 +1000

    add IServiceException Interface to make the Error exception extendable

--- BuberDinner.Application/Common/Errors/DuplicateEmailException.cs
+++ BuberDinner.Application/Common/Errors/DuplicateEmailException.cs
@@ -2,9 +2,9 @@ using System.Net;
 
 namespace BuberDinner.Application.Common.Errors;
 
-public class DuplicateEmailException : Exception 
+public class DuplicateEmailException : Exception , IServiceException
 {
-  // public HttpStatusCode StatusCode => HttpStatusCode.Conflict;
+  public HttpStatusCode StatusCode => HttpStatusCode.Conflict;
 
-  // public string ErrorMessage => "Email already exsits 01";
+  public string ErrorMessage => "Email already exsits 01";
 }
